f673cbef2673ed0492f71b4b99ef78e02bff2215
Sevda BAGCI KARATAS <sevdabagcikaratas@gmail.com>
2023-05-01 12:57:35 +0200

    Access Modifier

---
+++ src/day22_StringBuilder_AccessModifier/C05_accessModifier.java
@@ -0,0 +1,69 @@
+package day22_StringBuilder_AccessModifier;
+
+public class C05_accessModifier {
+
+    C05_accessModifier(){
+
+
+    }
+
+    private String privateStr;
+    String defaultStr;
+    protected String protectedStr;
+    public String publicStr;
+
+    private static int privateStaticSayi;
+    static int defaultStaticSayi;
+    protected static int protectedStaticSayi;
+    public static int publicStaticSayi;
+
+    private void privateMethod(){
+
+    }
+    void defaultMethod(){
+
+    }
+    protected void protectedMethod(){
+
+    }
+    public void publicMethod(){
+
+    }
+    private static void privateStaticMethod(){
+
+    }
+    static void defaultStaticMethod(){
+
+    }
+    protected static void protectedStaticMethod(){
+
+    }
+    public static void publicStaticMethod(){
+        //tum static class uyeleri direk kullanilabilir
+        // static olmayan tum class uyeleri icin ise obje olustuurlmasi gerekir
+
+        C05_accessModifier obj = new C05_accessModifier();
+        System.out.println(obj.privateStr);
+        obj.defaultStaticMethod();
+        // static class uyelerini obje uzerinden kullanmaya gerek yoktur
+        // ama kullanmak istersek java itiraz etmez
+
+
+
+
+    }
+
+    public void classUyeKullanimi(){
+        // static olan veya olmayan tum class uyeleri burda kullanilabilir.
+        // cunku static lerin gecis ustunlugu var
+
+        System.out.println(privateStaticSayi);
+        publicStaticMethod();
+
+        System.out.println(protectedStr);
+        protectedMethod();
+
+
+
+    }
+}
---
+++ src/day_23_encapsulation_inheritance/C01_AccessModifierKullanimi.java
@@ -0,0 +1,16 @@
+package day_23_encapsulation_inheritance;
+
+import day22_StringBuilder_AccessModifier.C05_accessModifier;
+
+public class C01_AccessModifierKullanimi {
+    public static void main(String[] args) {
+
+        // baska package'daki bir class'dan class uyelerinin
+        // sadece public olanlari kullanabiliriz
+        // eger bulundugumuz class ile ulasmak istedigimiz class arasinda
+        // parent-child iliskisi varsa class uyelerine erisim mumkundur
+
+        System.out.println(C05_accessModifier.publicStaticSayi);
+        C05_accessModifier.publicStaticMethod();
+    }
+}
