9545387525da6ceb7a119598f3667e5ad0ce1cc7
Sevda BAGCI KARATAS <sevdabagcikaratas@gmail.com>
2023-05-04 12:56:52 +0200

    inheritance class uyelerini kullanma

---
+++ src/day25_inheritence/Araba.java
@@ -0,0 +1,13 @@
+package day25_inheritence;
+
+public class Araba {
+
+    Araba(){
+        System.out.println("Araba constructor calisti");
+    }
+    String model="Arabalarin modeli olur";
+    String motor= "Arabalarda motor olur";
+    String marka="Arabalarda marka olur";
+}
+
+
