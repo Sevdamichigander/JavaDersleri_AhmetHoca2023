eefcec183ae2e6e96dd02d883645df214705d14c
Sevda BAGCI KARATAS <sevdabagcikaratas@gmail.com>
2023-05-12 12:57:35 +0200

    Interface

---
+++ src/day28_Interface/C01_ConcreteChildClass.java
@@ -0,0 +1,25 @@
+package day28_Interface;
+
+import day27_Overriding_Polymorphism_AbstractClasses.C07_AbstractP;
+
+public class C01_ConcreteChildClass extends C07_AbstractP {
+   /* Abstract parent(lar)dan sonra gelen
+    ILK Concrete Child class
+    Parent abstract class(lar)da concrete'lestirilmemis olan
+    tum abstract method'lari
+    IMPLEMENT etmek ZORUNDADIR
+
+    concrete olan methodlar ise opsiyoneldir
+    isterseniz override edebilirsiniz
+     */
+
+    @Override
+    protected void method2() {
+
+    }
+
+    @Override
+    protected void method5() {
+
+    }
+}
---
+++ src/day28_Interface/C08_Childof_I03.java
@@ -0,0 +1,35 @@
+package day28_Interface;
+
+public class C08_Childof_I03 implements I03_BodysiOlanMethod {
+    @Override
+    public void method1() {
+
+    }
+
+    @Override
+    public int method2() {
+        return 0;
+    }
+
+    @Override
+    public String method3() {
+        return null;
+    }
+
+    @Override
+    public double method4() {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        // method5(); method5 static olmadigi icin , static olan main method icinde direk kullanilamaz
+        I03_BodysiOlanMethod.method6();
+
+    }
+
+    public void method7() {
+        method5();
+        I03_BodysiOlanMethod.method6();
+
+    }
+}
